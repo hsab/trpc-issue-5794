0ea9167e9bd09f6baa36ced2baf4567218f1fb17
hsab <hsab@users.noreply.github.com>
2024-06-14 09:19:05 -0700

    rm gc staletime

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,20 @@ import { trpc } from "../utils/trpc";
 export default function IndexPage() {
   const query = trpc.iterable.useQuery(undefined, {
     enabled: false,
-    gcTime: 0,
-    staleTime: 0,
   });
 
   return (
     <div style={styles}>
       <div>
-      <button onClick={() => query.refetch()}>Fetch</button>
-      <br />
-      <div>
-        tRPC Iterable:
-        {query.data?.length &&
-          query.data?.map((chunk, index) => (
-            <Fragment key={index}>{chunk}</Fragment>
-          ))}
-      </div>
+        <button onClick={() => query.refetch()}>Fetch</button>
+        <br />
+        <div>
+          tRPC Iterable:
+          {query.data?.length &&
+            query.data?.map((chunk, index) => (
+              <Fragment key={index}>{chunk}</Fragment>
+            ))}
+        </div>
       </div>
     </div>
   );
